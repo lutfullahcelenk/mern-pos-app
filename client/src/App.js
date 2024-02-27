5096ee300355c79168218cb1fa9cc839497b2043
Lutfullah ÇELENK <lutfullahclnk1104@gmail.com>
2024-02-27 19:23:48 +0100

    feat: antd and tailwind are added to project

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import { Button } from "antd";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+    return (
+        <div>
+            <Button type="primary">Primary Button</Button>
+            <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
+        </div>
+    );
+};
 
-export default App
+export default App;
